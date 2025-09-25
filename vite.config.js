67d653c35e8a0d5cfb359a27eb3daa1521637059
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-09-25 11:44:50 +0800

    added the base link

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://abderrahmaneBouzemlal.github.io/dashbord-tailwind-svelte/',
   plugins: [
     svelte(),
     tailwindcss(),
