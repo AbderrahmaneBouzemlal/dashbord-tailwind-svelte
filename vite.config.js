afb82f048498a68e57b1d03ded84593c4abe338d
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-09-25 12:30:17 +0800

    change the cofigurationof the images url

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: 'https://abderrahmaneBouzemlal.github.io/dashbord-tailwind-svelte/',
+  base: '/dashbord-tailwind-svelte/',
   plugins: [
     svelte(),
     tailwindcss(),
